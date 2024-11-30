fa92dcf84ced964535723072be9ab6fea6cc4e3b
Andres <andrestoro0303@gmail.com>
2024-11-29 19:51:08 -0500

    ajuste

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cue_swap/provider/auth_provider.dart';
 import 'package:cue_swap/provider/complaint_provider.dart';
 import 'package:cue_swap/provider/offer_provider.dart';
@@ -15,8 +17,17 @@ import 'package:cue_swap/services/notifications_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
   await LocalStorage.configurePrefs();
   Flurorouter.configureRoutes();
   runApp(const AppState());
